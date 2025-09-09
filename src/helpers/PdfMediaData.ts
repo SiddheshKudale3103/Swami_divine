444a60e795c7d5c391e87fc5924a02b6897fe411
Siddhesh <sidkudale18@gmail.com>
2025-09-09 13:40:25 +0530

    added large pdf

--- src/helpers/PdfMediaData.ts
+++ src/helpers/PdfMediaData.ts
@@ -2,4 +2,5 @@ export const PdfMediaData = [
   { src: "/media/pdf/Pithikapuram.pdf", title: "Booklet 1" },
   { src: "/media/pdf/BOOKLET_4_PAGES.pdf", title: "Booklet 2" },
   { src: "/media/pdf/Swami_Seva.pdf", title: "Booklet 3" },
+  { src: "/media/pdf/Swami_Seva_1.pdf", title: "Booklet 4" },
 ];
