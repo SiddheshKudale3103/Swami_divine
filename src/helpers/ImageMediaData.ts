0fba0e2ef858ba385625601c6ac32a4ba371dfe0
Siddhesh <sidkudale18@gmail.com>
2025-09-09 14:52:57 +0530

    displayed all images poster

--- src/helpers/ImageMediaData.ts
+++ src/helpers/ImageMediaData.ts
@@ -129,5 +129,557 @@ export const ImageMediaData = [
     src: "/media/seva_poster/Seva-photo-.jpg",
     alt: "ShreeSwamiSamarthSevaSarSangh_",
   },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+{
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+{
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+{
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+{
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpeg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+  
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+    {
+    kind: "image",
+    src: "/media/seva_poster/Seva-photo-.jpg",
+    alt: "ShreeSwamiSamarthSevaSarSangh_",
+  },
+
 
 ];
